d25d2e6abf015065ab61fd8144e3fadf99601140
Marianne Harrison <marianneharrison93@gmail.com>
2017-01-16 14:17:14 +1300

    Changed UUIDs from Bluetooth Base UUIDs to RIOT Project UUIDs.

--- app/src/main/java/thyeway/xyz/activitytracker/UUIDs.java
+++ app/src/main/java/thyeway/xyz/activitytracker/UUIDs.java
@@ -3,9 +3,9 @@ package thyeway.xyz.activitytracker;
 
 public final class UUIDs {
 
-    public static final String LUX_SERVICE = "00008420-0000-1000-8000-00805f9b34fb";
+    public static final String LUX_SERVICE = "74b10000-6134-4da9-abe8-bc9e3833e943";
 
-    public static final String LUX_VALUE = "00008422-1212-efde-1523-785feabcd123";
-    public static final String LUX_SEQUENCE_NUMBER = "00008423-1212-efde-1523-785feabcd123";
-
+    public static final String LUX_VALUE = "74b10001-6134-4da9-abe8-bc9e3833e943";
+    public static final String LUX_SEQUENCE_NUMBER = "74b10002-6134-4da9-abe8-bc9e3833e943";
+    public static final String LUX_DEVICE_NAME = "74b10003-6134-4da9-abe8-bc9e3833e943";
 }
